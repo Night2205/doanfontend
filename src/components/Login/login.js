164b192440fe5c03ad161c1c81734073c9a23d65
iuh-quanlykho <nguyennhathuy1761@gmail.com>
2024-10-01 20:44:15 -0700

    form login

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -2,25 +2,32 @@ import { button } from 'bootstrap';
 import './login.scss'
 const Login = (props) => {
     return (
-        <div className="login-container mt-3">
+        <div className="login-container">
             <div className="container">
                 <div className="row">
                     <div className="content-left col-7 d-none d-sm-block">
-                        <div className='brand'>
-                            IUH
+                        <div className='logo'>
+                            <img src="/logo_IUH.png" alt="Logo IUH" />
                         </div>
                         <div className='detail'>
                             Innovation-Unity-Humanity
                         </div>
                     </div>
-                    <div className="content-right green col-12 col-sm-5 d-flex flex-column gap-3 py-3">
-                        <input type='text' className='form-control' placeholder='Email or phone number' />
-                        <input type='password' className='form-control' placeholder='Password' />
-                        <button className='btn btn-primary btn-lg'>login</button>
-                        <span className='text-center'>Forgot your password?</span>
-                        <hr />
-                        <div className='text-center'>
-                            <button className='btn btn-success'>Creat e new account</button>
+                    <div className='right-container col-12 col-sm-5'>
+                        <div className="content-right  d-flex flex-column gap-3 py-3">
+                            <div className='logo d-sm-none'>
+                                <img src="/logo_IUH.png" alt="Logo IUH" />
+                            </div>
+                            <input type='text' className='form-control' placeholder='Email or phone number' />
+                            <input type='password' className='form-control' placeholder='Password' />
+                            <button className='btn btn-primary btn-lg'>login</button>
+                            <span className='text-center'>
+                                <a className='forgot-password' href='#'>Forgot your password?</a>
+                            </span>
+                            <hr />
+                            <div className='text-center'>
+                                <button className='btn btn-success'>Create new account</button>
+                            </div>
                         </div>
                     </div>
                 </div>
